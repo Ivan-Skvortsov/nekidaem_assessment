e8f21ac89e0f3303274f5eb40aa263bc545be6d8
Ivan Skvortsov <pprofcheg@gmail.com>
2023-08-09 20:21:26 +0300

    created blog serializer and viewset

--- apps/blogs/serializers.py
+++ apps/blogs/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from apps.blogs.models import Post
+from apps.blogs.models import Blog, Post
 
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ["title", "text", "created_at", "blog"]
+
+
+class BlogSeriazlier(serializers.ModelSerializer):
+    class Meta:
+        models = Blog
+        fields = "__all__"  # TODO
--- apps/blogs/views.py
+++ apps/blogs/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
-from apps.blogs.models import Post
-from apps.blogs.serializers import PostSerializer
+from apps.blogs.models import Blog, Post
+from apps.blogs.serializers import BlogSeriazlier, PostSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class BlogViewSet(viewsets.ModelViewSet):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSeriazlier
