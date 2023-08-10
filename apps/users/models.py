0852b306b5c84f39f7fd7d7ff0e815448ceb586a
Ivan Skvortsov <pprofcheg@gmail.com>
2023-08-10 10:08:02 +0300

    removed SeenPosts model, recreated migrations

--- apps/users/models.py
+++ apps/users/models.py
@@ -7,6 +7,7 @@ from apps.blogs.models import Blog, Post
 class User(AbstractUser):
     blog = models.OneToOneField(Blog, related_name="owner", on_delete=models.CASCADE, verbose_name="Блог пользователя")
     follows = models.ManyToManyField(Blog, related_name="followed_by", blank=True, verbose_name="Подписки пользователя")
+    seen_posts = models.ManyToManyField(Post, related_name="seen_by", blank=True, verbose_name="Просмотренные посты")
 
     def save(self, *args, **kwargs):
         self.blog = Blog.objects.create()
@@ -15,14 +16,3 @@ class User(AbstractUser):
     def delete(self, *args, **kwargs):
         self.blog.delete()
         super().delete(*args, **kwargs)
-
-
-class SeenPosts(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-
-    class Meta:
-        constraints = [models.UniqueConstraint(fields=["user", "post"], name="unique_seen_post_entry")]
-
-    def __str__(self):
-        return f"Просмотр поста {self.post} пользователем {self.user}"
